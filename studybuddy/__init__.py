01ed09f3e3b9bcdb63d6f1cb11cfff8d89db8ba1
Daniel Xiao <daniel.zhu.xiao@gmail.com>
2026-05-27 16:44:57 -0700

    fixed app.py and fix join/leave member count

--- studybuddy/__init__.py
+++ studybuddy/__init__.py
@@ -27,6 +27,6 @@ def create_app():
 
     @app.errorhandler(404)
     def page_not_found(error):
-        return render_template("404.html", error=error), 404
+        return render_template("404error.html", error=error), 404
 
     return app
