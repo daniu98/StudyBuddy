53a5005a1b31dc5dc3ebfa71611cc673e5292d11
Jason <jasonzhou2025@g.ucla.edu>
2026-06-02 19:21:32 -0700

    refactoring (compress url checks)

--- database_test.py
+++ database_test.py
@@ -34,7 +34,6 @@ def client(app):
 def runner(app):
     return app.test_cli_runner()
 
-
 def test_signup(client, init_db):
     with client:
         init = client.get("/")
@@ -43,37 +42,32 @@ def test_signup(client, init_db):
             "email": "5newplaceholder@placeholder",
             "password": "short",
         }, follow_redirects=True)
-        assert len(failed_signup.history) == 1
-        assert failed_signup.request.path == url_for("auth.signup"), "Redirect on failed signup (password too short)"
+        assert check_redirect(failed_signup, url_for("auth.signup")), "Redirect on failed signup (password too short)"
         
         failed_signup = client.post(url_for("auth.signup"), data={
             "email": "5newplaceholder@placeholder",
             "password": "short",
         }, follow_redirects=True)
-        assert len(failed_signup.history) == 1
-        assert failed_signup.request.path == url_for("auth.signup"), "Redirect on failed signup (no username)"
+        assert check_redirect(failed_signup, url_for("auth.signup")), "Redirect on failed signup (no username)"
         
         failed_signup = client.post(url_for("auth.signup"), data={
             "name": "Placeholder5",
             "password": "short",
         }, follow_redirects=True)
-        assert len(failed_signup.history) == 1
-        assert failed_signup.request.path == url_for("auth.signup"), "Redirect on failed signup (no email)"
+        assert check_redirect(failed_signup, url_for("auth.signup")), "Redirect on failed signup (no email)"
         
         failed_signup = client.post(url_for("auth.signup"), data={
             "name": "Placeholder5",
             "email": "5newplaceholder@placeholder",
         }, follow_redirects=True)
-        assert len(failed_signup.history) == 1
-        assert failed_signup.request.path == url_for("auth.signup"), "Redirect on failed signup (no password)"
+        assert check_redirect(failed_signup, url_for("auth.signup")), "Redirect on failed signup (no password)"
         
         successful_signup = client.post(url_for("auth.signup"), data={
             "name": "Placeholder5",
             "email": "5newplaceholder@placeholder",
             "password": "Placeholder",
         }, follow_redirects=True)
-        assert len(successful_signup.history) == 1
-        assert successful_signup.request.path == url_for("auth.profile"), "Redirect on successful user creation"
+        assert check_redirect(successful_signup, url_for("auth.profile")), "Redirect on successful user creation"
         
         conn = get_db()
         row = conn.execute(
@@ -86,8 +80,7 @@ def test_login_logout(client, init_db):
     with client:
         login_placeholder(client)
         exit = client.get(url_for("auth.logout"), follow_redirects=True)
-        assert len(exit.history) == 1
-        assert exit.request.path == url_for("main.home"), "Successful logout and redirect"
+        assert check_redirect(exit, url_for("main.home")), "Successful logout and redirect"
         response_blocked = client.get(url_for("auth.profile"))
         assert response_blocked.status_code != 200, "Login required restriction works"
         
@@ -95,21 +88,18 @@ def test_login_logout(client, init_db):
             "email": "blah@blah",
             "password": "PL",
         }, follow_redirects=True)
-        assert len(failed_login.history) == 1
-        assert failed_login.request.path == url_for("auth.login"), "Failed login (wrong password) and redirect"
+        assert check_redirect(failed_login, url_for("auth.login")), "Failed login (wrong password) and redirect"
 
         failed_login = client.post(url_for("auth.login"), data={
             "password": "Placeholder",
         }, follow_redirects=True)
-        assert len(failed_login.history) == 1
-        assert failed_login.request.path == url_for("auth.login"), "Failed login (no email) and redirect"
+        assert check_redirect(failed_login, url_for("auth.login")), "Failed login (no email) and redirect"
         
         login = client.post(url_for("auth.login"), data={
             "email": "blah@blah",
             "password": "Placeholder",
         }, follow_redirects=True)
-        assert len(login.history) == 1
-        assert login.request.path == url_for("main.home"), "Successful login and redirect"
+        assert check_redirect(login, url_for("main.home")), "Successful login and redirect"
 
         profile_access = client.get(url_for("auth.profile"))
         assert profile_access.status_code == 200, "User profile accessible after login"
@@ -124,8 +114,7 @@ def test_profile(client, init_db):
         update_courses = client.post(url_for("auth.profile"), data={
             "course_ids": ["2"],
         }, follow_redirects=True)
-        assert len(update_courses.history) == 1
-        assert update_courses.request.path == url_for("auth.profile"), "Successful course list update and redirect"
+        assert check_redirect(update_courses, url_for("auth.profile")), "Successful course list update and redirect"
         
         conn = get_db()
         user = conn.execute("SELECT * FROM users WHERE email LIKE 'blah@blah'").fetchone()
@@ -152,13 +141,12 @@ def test_profile(client, init_db):
 def test_group_create(client, init_db):
     with client:
         init = client.get("/")
-        response_reject = client.post(url_for("groups.create_study_group"), data={
+        creation_reject = client.post(url_for("groups.create_study_group"), data={
             "title": "LePlaceholder",
             "max_members": 8,
             "selected_course_ids": "",
         }, follow_redirects=True)
-        assert len(response_reject.history) == 1
-        assert response_reject.request.path == url_for("auth.login"), "Accessing group creation without login"
+        assert check_redirect(creation_reject, url_for("auth.login")), "Accessing group creation without login"
         
         login_placeholder(client)
         create_group = client.post(url_for("groups.create_study_group"), data={
@@ -166,8 +154,7 @@ def test_group_create(client, init_db):
             "max_members": 8,
             "selected_course_ids": "",
         }, follow_redirects=True)
-        assert len(create_group.history) == 1
-        assert create_group.request.path == url_for("main.home"), "Successful group creation and redirect"
+        assert check_redirect(create_group, url_for("main.home")), "Successful group creation and redirect"
         
         conn = get_db()
         row = conn.execute(
@@ -198,12 +185,10 @@ def test_join_leave_group(client, init_db):
             "SELECT * FROM study_groups WHERE title LIKE 'LePlaceholder2'",
         ).fetchone()
         entry = client.post(url_for("groups.join_group", group_id=row["id"]), follow_redirects=True)
-        assert len(entry.history) == 1
-        assert entry.request.path == url_for("groups.group_detail", group_id=row["id"]), "Successful group joining"
+        assert check_redirect(entry, url_for("groups.group_detail", group_id=row["id"])), "Successful group joining"
 
         exit = client.post(url_for("groups.leave_group", group_id=row["id"]), follow_redirects=True)
-        assert len(exit.history) == 1
-        assert exit.request.path == url_for("groups.browse_groups"), "Successful group leaving"
+        assert check_redirect(exit, url_for("groups.browse_groups")), "Successful group leaving"
 
         conn.commit()
         conn.close()
@@ -243,6 +228,9 @@ def login_placeholder(client):
         "email": "blah@blah",
         "password": "Placeholder",
     })
+
+def check_redirect(response, intended_redirect):
+    return len(response.history) == 1 and response.request.path == intended_redirect
 
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
